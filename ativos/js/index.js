e24dbccd0abdc4015d50211f7f703cf0b2794d92
Thiago Menezes <thiago.cria.oliveira@gmail.com>
2017-11-27 22:07:45 -0300

    Apaga Tudo

---
+++ ativos/js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+  $('.menu-hamburger_fundo').click(function () {
+    $('.linha-1').toggleClass('linha-1_fechar');
+    $('.linha-2').toggleClass('linha-2_fechar');
+    $('.linha-3').toggleClass('linha-3_fechar');
+  });
+});
